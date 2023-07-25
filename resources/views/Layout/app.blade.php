c72df45c816b70e66fddca155fcbf71cae47bc16
Azran Hossain <mr69751@gmail.com>
2023-07-25 10:09:50 +0600

    res logo, auth nav

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -1,4 +1,15 @@
 @include('Layout.header')
-@include('Layout.navbar')
+@guest
+    <nav class="flex items-center md:justify-normal justify-center px-5 py-2 border-b-[1px]">
+        <div class="flex gap-5">
+            <a href="{{ route('dashboard') }}" class="flex gap-5">
+                @svg('fas-blog', ['class' => 'w-8 h-8'])
+            </a>
+        </div>
+    </nav>
+@endguest
+@auth
+    @include('Layout.navbar')
+@endauth
 @yield('content')
 @include('Layout.footer')
